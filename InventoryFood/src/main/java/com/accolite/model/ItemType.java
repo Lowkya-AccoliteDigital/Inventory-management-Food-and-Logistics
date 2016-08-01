631faf0f9a79cb3644b29413e24f584dc32c9518
Loghitha <loghithavani.sundaramoorthi@accoliteindia.com>
2016-08-01 18:41:33 +0530

    Entire project is committed

---
+++ InventoryFood/src/main/java/com/accolite/model/ItemType.java
@@ -0,0 +1,27 @@
+package com.accolite.model;
+
+public class ItemType {
+	private Integer typeID;
+	private String type;
+	private String subType;
+	public Integer getTypeID() {
+		return typeID;
+	}
+	public void setTypeID(Integer typeID) {
+		this.typeID = typeID;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSubType() {
+		return subType;
+	}
+	public void setSubType(String subType) {
+		this.subType = subType;
+	}
+	
+	
+}
