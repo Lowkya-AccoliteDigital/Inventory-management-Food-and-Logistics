fb02ff5575e3e8d3198759b709c8c715887d979d
Loghitha <loghithavani.sundaramoorthi@accoliteindia.com>
2016-08-04 20:29:37 +0530

    Constants updated with email expiry query by loghitha

--- InventoryFood/src/main/java/com/accolite/resources/Constants.java
+++ InventoryFood/src/main/java/com/accolite/resources/Constants.java
@@ -17,6 +17,8 @@ public class Constants {
 
 	public static final String ADD_ITEM_TYPE = "Insert into dbo.type(type,subtype)values(?,?);";
 
+	public static final String EMAIL_EXPIRY = "select log.itemID, item.itemName from log join item on"
+			                                  +" log.itemID=item.itemID where log.dateofexpiry <= GETDATE();";
 	//public static final String VIEW_SUMMARY = ";with log11 as( select log1.itemID itemID,item.itemName,sum(log1.Quantity) total from dbo.log log1 join item on log1.itemID=item.itemID   where log1.io='i' and log1.dateofpurchase between ? and ? group  by log1.itemID,item.itemName),logFinal as(select item.itemID,item.itemName from item),log12 as( select log2.itemID,item.itemName,sum(Quantity) used from dbo.log log2 join item on item.itemID=log2.itemID where log2.io='o' and log2.dateofpurchase between '?' and '?' group by log2.itemID,item.itemName) select log11.itemID,log11.itemName,total,IsNull(used,0) as used, IsNull(total-used,0) as remaining  from log11 left outer join log12 on log11.itemID=log12.itemID;";
 
 }
