4ebbac87f9cf6d266fe78f6e634292dbba97b02a
mominaccolite <momin.yadav@accoliteindia.com>
2016-08-04 14:58:11 +0530

    Delete insert in itemCollection.sql

--- Database/insert in itemCollection.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE TABLE  Inventory.dbo.itemCollection
-select *from Inventory.dbo.itemCollection
-insert into Inventory.dbo.itemCollection([itemID],[subItemID]) values(7,4)
-insert into Inventory.dbo.itemCollection([itemID],[subItemID]) values(7,5)
-insert into Inventory.dbo.itemCollection([itemID],[subItemID]) values(7,6)
