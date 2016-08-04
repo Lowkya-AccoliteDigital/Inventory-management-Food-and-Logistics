f4a75eff60971c560acb51df46f239d61221ce9f
Lowkya123 <lowkya.v@accoliteindia.com>
2016-08-04 19:43:14 +0530

    model for sending emails in case of expiry by Lowkya

---
+++ InventoryFood/src/main/java/com/accolite/model/EmailExpiry.java
@@ -0,0 +1,19 @@
+package com.accolite.model;
+
+public class EmailExpiry {
+
+	int itemID;
+	String itemName;
+	public int getItemID() {
+		return itemID;
+	}
+	public void setItemID(int itemID) {
+		this.itemID = itemID;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+}
