60bb2cda4718b4bec88e8f94c774bdee596d9785
mominaccolite <momin.yadav@accoliteindia.com>
2016-08-04 14:54:01 +0530

    Add files via upload

---
+++ Database/insert in log.sql
@@ -0,0 +1,94 @@
+TRUNCATE TABLE  Inventory.dbo.log
+select *from Inventory.dbo.log
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(1,15,'1 Ltr','i','2016-08-01','2016-10-22',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(2,15,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(3,15,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(4,15,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(5,15,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(6,15,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(7,70,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(8,15,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(9,15,'1.5 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(10,65,'500 ml','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(11,75,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(12,20,'150 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(13,8,'350 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(14,12,'350 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(15,10,'180 gm','i','2016-08-01','2016-10-26',0)
+
+
+
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(16,20,'150 gm','i','2016-08-01','2016-10-22',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(17,16,'1.25 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(18,16,'1.25 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(19,15,'1 Kg','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(20,16,'300 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(21,15,'700 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(22,70,'90 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(23,30,'150 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(24,20,'127.4 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(25,20,'18 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(26,20,'15 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(27,20,'18 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(28,20,'55 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(29,25,'100 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(30,20,'550 gm','i','2016-08-01','2016-10-26',0)
+
+
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(31,5,'462 gm','i','2016-08-01','2016-10-22',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(32,5,'462 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(33,3,'400 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(34,3,'500 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(35,3,'500 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(36,5,'500 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(37,12,'200 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(38,90,'1 Ltr','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(39,15,'100 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(40,5,'500 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(41,5,'275 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(42,10,'300 ml','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(43,10,'50 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(44,3,'300 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(45,5,'500 ml','i','2016-08-01','2016-10-26',0)
+
+
+
+
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(46,5,'8 Pcs','i','2016-08-01','2016-10-22',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(47,3,'900 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(48,3,'500 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(49,5,'7.5*2.5 cm 1 pc','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(50,10,'500 ml','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(51,5,'250 ml','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(52,5,'500 ml','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(53,5,'500 ml','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(54,15,'82 gm','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(55,10,'1 Kg','i','2016-08-01','2016-10-26',0)
+insert into Inventory.dbo.log([itemID],[Quantity],[unit],[io],[dateofpurchase],[dateofexpiry],[expiryflag])values(56,75,'70 gm','i','2016-08-01','2016-10-26',0)
+
+
+
+
+
+
+
+
+--DELETE FROM Inventory.dbo.log
+--WHERE [itemID]=7
+
+
+
+
+CREATE TRIGGER trg2 ON Inventory.dbo.log
+FOR UPDATE
+AS
+    UPDATE Inventory.dbo.log
+    SET expiryflag=1
+	--where dateofexpiry='2016-07-26'
+    WHERE ((select convert(date, getdate()))> dateofexpiry)
+	--where SELECT DATEDIFF(day,(select convert(date, getdate())),dateofexpiry)< 0
+
+--select convert(date, getdate())
+
+select * from Inventory.dbo.log
+WHERE ((select convert(date, getdate()))> dateofexpiry)
