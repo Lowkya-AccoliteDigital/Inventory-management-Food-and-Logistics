c732209baf0a080e79d53fef3aa615fd672ef27e
Lowkya123 <lowkya.v@accoliteindia.com>
2016-08-04 14:53:32 +0530

    summary model for view summary query by lowkya

---
+++ InventoryFood/src/main/java/com/accolite/model/Summary.java
@@ -0,0 +1,113 @@
+package com.accolite.model;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Summary.
+ */
+public class Summary {
+
+	/** The item ID. */
+	int itemID;
+	
+	/** The item name. */
+	String itemName;
+	
+	/** The total. */
+	int total;
+	
+	/** The used. */
+	int used;
+	
+	/** The remaining. */
+	int remaining;
+	
+	/**
+	 * Gets the item ID.
+	 *
+	 * @return the item ID
+	 */
+	public int getItemID() {
+		return itemID;
+	}
+	
+	/**
+	 * Sets the item ID.
+	 *
+	 * @param itemID the new item ID
+	 */
+	public void setItemID(int itemID) {
+		this.itemID = itemID;
+	}
+	
+	/**
+	 * Gets the item name.
+	 *
+	 * @return the item name
+	 */
+	public String getItemName() {
+		return itemName;
+	}
+	
+	/**
+	 * Sets the item name.
+	 *
+	 * @param itemName the new item name
+	 */
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	
+	/**
+	 * Gets the total.
+	 *
+	 * @return the total
+	 */
+	public int getTotal() {
+		return total;
+	}
+	
+	/**
+	 * Sets the total.
+	 *
+	 * @param total the new total
+	 */
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	/**
+	 * Gets the used.
+	 *
+	 * @return the used
+	 */
+	public int getUsed() {
+		return used;
+	}
+	
+	/**
+	 * Sets the used.
+	 *
+	 * @param used the new used
+	 */
+	public void setUsed(int used) {
+		this.used = used;
+	}
+	
+	/**
+	 * Gets the remaining.
+	 *
+	 * @return the remaining
+	 */
+	public int getRemaining() {
+		return remaining;
+	}
+	
+	/**
+	 * Sets the remaining.
+	 *
+	 * @param remaining the new remaining
+	 */
+	public void setRemaining(int remaining) {
+		this.remaining = remaining;
+	}
+}
