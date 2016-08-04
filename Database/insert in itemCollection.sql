43c0f63b35f55a36bb8650537f0c386703b1020e
mominaccolite <momin.yadav@accoliteindia.com>
2016-08-04 14:59:45 +0530

    Momin Yadav

---
+++ Database/insert in itemCollection.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE  Inventory.dbo.itemCollection
+select *from Inventory.dbo.itemCollection
+insert into Inventory.dbo.itemCollection([itemID],[subItemID]) values(7,4)
+insert into Inventory.dbo.itemCollection([itemID],[subItemID]) values(7,5)
+insert into Inventory.dbo.itemCollection([itemID],[subItemID]) values(7,6)
---
+++ Database/typetable.sql
@@ -0,0 +1,15 @@
+TRUNCATE TABLE  Inventory.dbo.type
+select *from Inventory.dbo.type
+insert into Inventory.dbo.type([type],[subtype]) values('snacks','biscuits')
+insert into Inventory.dbo.type([type],[subtype]) values('stationary','pen')
+insert into Inventory.dbo.type([type],[subtype]) values('stationary','books')
+insert into Inventory.dbo.type([type],[subtype]) values('stationary','laptop')
+insert into Inventory.dbo.type([type],[subtype]) values('stationary','battery')
+
+insert into Inventory.dbo.type([type],[subtype]) values('stationary','bottles')
+
+insert into Inventory.dbo.type([type],[subtype]) values('stationary','AUkit')
+
+
+
+
