838e5d5a217b05343099f07dd2d522ec8e57442b
Loghitha <loghithavani.sundaramoorthi@accoliteindia.com>
2016-08-05 09:45:36 +0530

    Item controller committed with Logger
    Done by Loghitha

--- InventoryFood/src/main/java/com/accolite/controller/ItemController.java
+++ InventoryFood/src/main/java/com/accolite/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.accolite.controller;
 import java.text.ParseException;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.accolite.model.Item;
 import com.accolite.model.Summary;
+import com.accolite.resources.Logging;
 import com.accolite.service.ItemService;
 
 @Controller
 public class ItemController {
-
+	final static Logger logger = Logger.getLogger(ItemController.class);
 	@Autowired
 	private ItemService itemService;
 
@@ -24,6 +26,13 @@ public class ItemController {
 	@RequestMapping(value = "/items/{visibility}", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Item> getItems(@PathVariable("visibility") Integer visibility) {
+		if(logger.isDebugEnabled()){
+			logger.debug("Request for displaying all items is sent....");
+		}
+		
+		if(logger.isInfoEnabled()){
+			logger.info("Request for displaying all items is sent....");
+		}
 		return itemService.getItems(visibility);
 	}
 
@@ -31,6 +40,15 @@ public class ItemController {
 	@RequestMapping(value = "/addItem/{itemName}/{typeId}", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public void addItem(@PathVariable("itemName") String name, @PathVariable("typeId") Integer typeId) {
+		//Log file generation
+		if(logger.isDebugEnabled()){
+			logger.debug("Request for adding item "+name+" is sent....");
+		}
+		
+		if(logger.isInfoEnabled()){
+			logger.info("Request for adding item "+name+" is sent....");
+		}
+		
 		itemService.addItem(name, typeId);
 	}
 
@@ -38,6 +56,13 @@ public class ItemController {
 	@RequestMapping(value = "/removeItem/{itemName}", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public void removeItem(@PathVariable("itemName") String name) {
+		if(logger.isDebugEnabled()){
+			logger.debug("Request for removing item "+name+" is sent....");
+		}
+		
+		if(logger.isInfoEnabled()){
+			logger.info("Request for removing item "+name+" is sent....");
+		}
 		itemService.removeItem(name);
 	}
 
@@ -45,6 +70,13 @@ public class ItemController {
 	@RequestMapping(value = "/addItemType/{typeName}/{subtypeName}", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public void addItemType(@PathVariable("typeName") String type, @PathVariable("subtypeName") String subtype) {
+		if(logger.isDebugEnabled()){
+			logger.debug("Request for adding new item type "+subtype+" is sent....");
+		}
+		
+		if(logger.isInfoEnabled()){
+			logger.info("Request for adding new item type "+subtype+" is sent....");
+		}
 		itemService.addItemType(type, subtype);
 	}
 
@@ -52,6 +84,13 @@ public class ItemController {
 	@ResponseBody
 	public List<Summary> viewSummary(@PathVariable("fromDate") String startdate, @PathVariable("toDate") String enddate)
 			throws ParseException {
+		if(logger.isDebugEnabled()){
+			logger.debug("Request for displaying summary is sent....");
+		}
+		
+		if(logger.isInfoEnabled()){
+			logger.info("Request for displaying summary is sent....");
+		}
 		return itemService.viewSummary(startdate, enddate);
 	}
 
