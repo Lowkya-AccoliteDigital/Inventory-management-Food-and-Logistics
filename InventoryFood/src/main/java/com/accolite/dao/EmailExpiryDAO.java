58d148fb525cf5929e64b1fd3bdbd5d1dedc1b4f
Lowkya123 <lowkya.v@accoliteindia.com>
2016-08-05 11:44:20 +0530

    commit for @repository by lowkya

--- InventoryFood/src/main/java/com/accolite/dao/EmailExpiryDAO.java
+++ InventoryFood/src/main/java/com/accolite/dao/EmailExpiryDAO.java
@@ -2,7 +2,6 @@ package com.accolite.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,11 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Repository;
 
 import com.accolite.model.EmailExpiry;
-import com.accolite.model.Item;
 import com.accolite.resources.Constants;
 
+@Repository
 public class EmailExpiryDAO {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
